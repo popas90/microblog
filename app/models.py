9f301d62f97fa3ee506ac4c792816c6d48de1b9d
popas90 <bogdititupopa@gmail.com>
2015-11-07 20:55:34 +0200

    part 5 done again

--- app/models.py
+++ app/models.py
@@ -10,6 +10,24 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(self.id)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
